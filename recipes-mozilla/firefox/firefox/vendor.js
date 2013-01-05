9a15050735baf0541035e0055468af6df034c30c
Otavio Salvador <otavio@ossystems.com.br>
2013-01-04 22:18:26 -0200

    firefox: Update to 10.0.11esr
    This updates the recipe in sync with Debian 10.0.11esr-1 package.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- recipes-mozilla/firefox/firefox/vendor.js
+++ recipes-mozilla/firefox/firefox/vendor.js
@@ -1,4 +1,6 @@
 pref("general.useragent.compatMode.firefox", true);
+pref("distribution.searchplugins.defaultLocale", "en-US");
+// Forbid application updates
 lockPref("app.update.enabled", false);
 lockPref("extensions.update.enabled", false);
 lockPref("extensions.autoDisableScopes", 10);
